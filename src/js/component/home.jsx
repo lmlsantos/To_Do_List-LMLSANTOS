403a7509c6286ef0bb845d276385bff205705a85
lmlsantos <126293567+lmlsantos@users.noreply.github.com>
2023-06-08 18:57:56 +0000

    todo list LMLSANTOS

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
+//<button type ="submit" onClick={()=>{
+						//if (todo ===""){
+							//alert("Insert a task!");
+						//} else {
+							//setToDoList([...todoList, todo])
+							//setToDo("");
+						//}	
+					//}}>Add task</button>
+
+
+
 
 //create your first component
 const Home = () => {
+	
+	const [todo, setToDo] = useState ("");
+	const [todoList, setToDoList] = useState([]);
+	
+	
+	// delete a task
+	const handleDelete = (deleteItem) => {
+		
+		const newTask = todoList.filter((todoItem) => todoItem !== deleteItem);
+		setToDoList(newTask);
+	}
+
+	// the function that replaces the button
+	const handleKeyDown = (e) => {
+		
+		if(e.key === "Enter"){
+			if (todo ===""){
+				alert("Insert a task!");
+			} else {
+				setToDoList([...todoList, todo])
+				setToDo("");
+			}	
+		}
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		
+		<div className="container">	
+			<h2>todos</h2>
+			
+			<div className="notepad">				
+				<div className="inputArea">
+					<input type="text"
+						value={todo}
+						onChange={(e) => {
+						setToDo(e.target.value)
+						}}
+						onKeyDown={handleKeyDown}
+						placeholder="What needs to be done?"/>		
+				</div>
+			<div className="text-center">
+  				<ul className="listItems">
+    				{todoList.length !== 0 &&
+      				todoList.map((todoItem) => (
+        			<li key={todoItem}>
+          			<div className="listItemContainer">
+            			<span className="todoItem">{todoItem}</span>
+            			<i className="fas fa-times" onClick={() => handleDelete(todoItem)}></i>   						
+          			</div>
+        			</li>
+      				))}
+  				</ul>
+			</div>
+
+			<div className="bottom">
+					<p className="mb-0 mt-0"> {todoList.length>0 ? todoList.length: ""} item left</p>
+			</div>	
+			</div>
+			<div className="sheet1"></div>
+			<div className="sheet2"></div>
+		</div>	
 	);
 };
 
